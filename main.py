13a478245abb262bf97b65ebbe15ca7235b4f316
Vistarikov321 <94439203+Vistarikov321@users.noreply.github.com>
2021-11-19 15:53:44 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+ #   print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
+
+print(2 + 4)
+
+print(9 / 4)
+
+
+
+a = 10
+b = a + 5
+print('10 + 5 = ', b)
+name = "Viktor"
+surname = 'Starikov'
+print('Hi man', name, surname)
+
+# name = input("What is your name? ")
+
+
